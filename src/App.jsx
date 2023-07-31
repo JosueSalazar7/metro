5c4fb8fda73919980b1bc89401a9853699fc19b1
JosueSalazar7 <josue.salazar@epn.edu.ec>
2023-07-31 16:38:45 -0500

    todo

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./paginas/LandingPage";
+import Login from "./layout/Login";
+import FormularioLogin from "./paginas/FormularioLogin";
+import Dashboard from "./layout/Dashboard";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+    <Routes>
+
+      <Route path="/" element={<LandingPage />}>
+      </Route>
+
+      <Route path="/login" element={<Login />}>
+        <Route index element={<FormularioLogin />} />
+      </Route>
+
+      <Route path="/fundamentos" element={<Dashboard />}>
+      </Route>
+
+    </Routes>
+  </BrowserRouter>
+
+  )
+
+}
+
+export default App
